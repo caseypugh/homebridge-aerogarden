3000d81945a44173679d16d62343c696e88fb544
Casey Pugh <156097+caseypugh@users.noreply.github.com>
2020-11-30 22:17:34 -0500

    super basic power toggle for aerogarden

--- src/platform.ts
+++ src/platform.ts
@@ -54,24 +54,20 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     // EXAMPLE ONLY
     // A real plugin you would discover accessories from the local network, cloud services
     // or a user-defined array in the platform config.
-    const exampleDevices = [
+    const devices = [
       {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
+        uniqueId: 'aerogarden-bathroom',
+        displayName: 'Garden',
+      }
     ];
 
     // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+    for (const device of devices) {
 
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+      const uuid = this.api.hap.uuid.generate(device.uniqueId);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
@@ -100,10 +96,10 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
         }
       } else {
         // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+        this.log.info('Adding new accessory:', device.displayName);
 
         // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+        const accessory = new this.api.platformAccessory(device.displayName, uuid);
 
         // store a copy of the device object in the `accessory.context`
         // the `context` property can be used to store any data about the accessory you may need
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -14,10 +14,12 @@ export class ExamplePlatformAccessory {
    * These are just used to create a working example
    * You should implement your own code to track the state of your accessory
    */
-  private exampleStates = {
+  private states = {
     On: false,
     Brightness: 100,
   };
+
+  private timeout;
 
   constructor(
     private readonly platform: ExampleHomebridgePlatform,
@@ -26,7 +28,7 @@ export class ExamplePlatformAccessory {
 
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Aerogarden')
       .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
       .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
 
@@ -36,7 +38,7 @@ export class ExamplePlatformAccessory {
 
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.displayName);
 
     // each service must implement at-minimum the "required characteristics" for the given service type
     // see https://developers.homebridge.io/#/service/Lightbulb
@@ -47,8 +49,8 @@ export class ExamplePlatformAccessory {
       .on('get', this.getOn.bind(this));               // GET - bind to the `getOn` method below
 
     // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .on('set', this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+    // this.service.getCharacteristic(this.platform.Characteristic.Brightness)
+    //   .on('set', this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
 
 
     /**
@@ -63,11 +65,11 @@ export class ExamplePlatformAccessory {
      */
 
     // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
+    // const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
+    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
 
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
+    // const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
+    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
 
     /**
      * Updating characteristics values asynchronously.
@@ -78,18 +80,18 @@ export class ExamplePlatformAccessory {
      * the `updateCharacteristic` method.
      * 
      */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
+    // let motionDetected = false;
+    // setInterval(() => {
+    //   // EXAMPLE - inverse the trigger
+    //   motionDetected = !motionDetected;
 
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
+    //   // push the new value to HomeKit
+    //   // motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
+    //   // motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
 
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+    //   this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
+    //   this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
+    // }, 10000);
   }
 
   /**
@@ -99,12 +101,76 @@ export class ExamplePlatformAccessory {
   setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
 
     // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+    this.states.On = value as boolean;
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+    let update_url = 'http://ec2-54-86-39-88.compute-1.amazonaws.com:8080/'
 
-    // you must call the callback function
-    callback(null);
+    this.platform.log.debug('Toggle ', value ? 'On' : 'Off');
+    // this.platform.log.debug('Brightness ->', this.states.Brightness);
+
+    if (value == false && this.states.Brightness == 100) {
+      this.togglePower().then(res => 
+        { 
+          setTimeout(this.togglePower.bind(this), 300);
+        }
+      );
+    }
+    else if (value == false && this.states.Brightness < 100 && this.states.Brightness > 0)
+    {
+      this.togglePower();
+    }
+    else if (value == true && this.states.Brightness == 0)
+    {
+      this.togglePower();
+    }
+
+    clearInterval(this.timeout);
+
+    this.timeout = setTimeout(function() {
+      callback(null);
+    }, 2000);
+  }
+
+  togglePower()
+  {
+    this.platform.log.debug('togglePower');
+    let params = {
+      airGuid: this.platform.config.macAddress,
+      chooseGarden: 0,
+      userID: this.platform.config.userID,
+      plantConfig: JSON.stringify({lightTemp: 1 })
+    };
+
+    return this.aerogarden('/api/Custom/UpdateDeviceConfig', params)
+        .then(res => {
+          let prevBrightness = this.states.Brightness;
+
+          if (this.states.Brightness == 100) {
+            this.states.On = true;
+            this.states.Brightness = 50;
+            this.service.updateCharacteristic(this.platform.Characteristic.On, true);
+            this.service.updateCharacteristic(this.platform.Characteristic.Brightness, 50);
+          }
+          else if (this.states.Brightness > 0 && this.states.Brightness < 100) {
+            this.states.On = false;
+            this.states.Brightness = 0;
+            this.service.updateCharacteristic(this.platform.Characteristic.On, false);
+            this.service.updateCharacteristic(this.platform.Characteristic.Brightness, 0);
+          }
+          else {
+            this.states.On = true;
+            this.states.Brightness = 100;
+            this.service.updateCharacteristic(this.platform.Characteristic.On, true);
+            this.service.updateCharacteristic(this.platform.Characteristic.Brightness, 100);
+          }
+
+          this.platform.log.debug('Changing brightness from ', prevBrightness, ' to ', this.states.Brightness);
+          
+        })
+        .catch(error => {
+          // this.platform.log.error('setOn', error);
+          // callback(null);
+        });
   }
 
   /**
@@ -123,14 +189,54 @@ export class ExamplePlatformAccessory {
   getOn(callback: CharacteristicGetCallback) {
 
     // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+    let isOn = this.states.On;
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
+    // let update_url = 'http://ec2-54-86-39-88.compute-1.amazonaws.com:8080/api/CustomData/QueryUserDevice'
 
-    // you must call the callback function
-    // the first argument should be null if there were no errors
-    // the second argument should be the value to return
-    callback(null, isOn);
+    this.aerogarden('/api/CustomData/QueryUserDevice', { userID: this.platform.config.userID})
+        .then(res => {
+          // this.platform.log.debug('getOn=', res.data);
+          this.platform.log.debug('lightStat=', res.data[0].lightStat);
+          isOn = res.data[0].lightStat == 1;
+
+          if (res.data[0].lightStat == 0) 
+          {
+            this.states.Brightness = 0;
+            this.service.updateCharacteristic(this.platform.Characteristic.Brightness, 0);
+          }
+          // else if (res.data[0].lightStat == 1 && this.states.Brightness == 0)
+          // {
+            // this.states.Brightness = 1;
+            // this.service.updateCharacteristic(this.platform.Characteristic.Brightness, 1);
+          // }
+
+          if (!isOn) {
+            callback(null, isOn);
+          }
+        })
+        .catch(error => {
+          this.platform.log.error('getOn', error);
+          callback(null, isOn);
+        });
+  }
+
+  aerogarden(api_path: string, params: object)
+  {
+    let base_url = 'http://ec2-54-86-39-88.compute-1.amazonaws.com:8080'
+
+    let config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'HA-Aerogarden/0.1'
+      }
+    };
+
+    const querystring = require('querystring');
+    const params_qs = querystring.stringify(params);
+
+    const axios = require('axios')
+    axios.defaults.headers.common = {};
+    return axios.post(base_url + api_path, params_qs, config)
   }
 
   /**
@@ -140,9 +246,44 @@ export class ExamplePlatformAccessory {
   setBrightness(value: CharacteristicValue, callback: CharacteristicSetCallback) {
 
     // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+    // this.states.Brightness = value as number;
 
     this.platform.log.debug('Set Characteristic Brightness -> ', value);
+
+    // this.aerogarden('/api/Custom/UpdateDeviceConfig', params)
+    //     .then(res => {
+    //       let prevBrightness = this.states.Brightness;
+
+    //       if (this.states.Brightness == 100) {
+    //         this.states.On = true;
+    //         this.states.Brightness = 50;
+    //         this.service.updateCharacteristic(this.platform.Characteristic.On, true);
+    //         this.service.updateCharacteristic(this.platform.Characteristic.Brightness, 50);
+    //       }
+    //       else if (this.states.Brightness == 50) {
+    //         this.states.On = false;
+    //         this.states.Brightness = 0;
+    //         this.service.updateCharacteristic(this.platform.Characteristic.On, false);
+    //         this.service.updateCharacteristic(this.platform.Characteristic.Brightness, 0);
+    //       }
+    //       else {
+    //         this.states.On = true;
+    //         this.states.Brightness = 100;
+    //         this.service.updateCharacteristic(this.platform.Characteristic.On, true);
+    //         this.service.updateCharacteristic(this.platform.Characteristic.Brightness, 100);
+    //       }
+
+    //       this.platform.log.debug('Changing brightness from ', prevBrightness, ' to ', this.states.Brightness);
+          
+    //       clearInterval(this.timeout);
+    //       this.timeout = setTimeout(function() {
+    //         callback(null);
+    //       }, 3000);
+    //     })
+    //     .catch(error => {
+    //       this.platform.log.error('setOn', error);
+    //       callback(null);
+    //     });
 
     // you must call the callback function
     callback(null);
